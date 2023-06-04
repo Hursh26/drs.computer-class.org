28b8ba17ba24ccea68cdc16eeba75be5e813ac65
Kuldeep Pandey <95524165+Hursh26@users.noreply.github.com>
2023-06-04 22:22:40 +0530

    initial commit
    Added files so that team can see and suggest changes while it is being developed

---
+++ js/main.js
@@ -0,0 +1,38 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.nav-bar').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.nav-bar').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1000, 'easeInOutExpo');
+        return false;
+    });
+
+})(jQuery);
